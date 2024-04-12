9a87a8bbb7bb3112a0608d66cc55354afb093b82
Aswathy05 <aswathyme1@gmail.com>
2024-04-12 21:44:59 +0530

    fuel volume finding

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from services import firebase
+import math
 
 app = Flask(__name__)
 
@@ -18,6 +19,41 @@ def get_vehicle():
         response = "Please provide VID"
     return jsonify(response)
 
+@app.route("/fuellevel")
+def fuel_level():
+    vid = request.args.get("vid")
+    level = request.args.get("h")
+    print(vid,level)
+    if vid != None and level!=None:
+        response = firebase.fuel_level(vid,level)
+        return "Fuel level set successfully"
+    else:
+        if vid == None:
+            response = "Please provide VID "
+        if level == None:
+            response = "Please provide level"
+        return response
+@app.route("/fuelvolume")
+def fuel_volume():
+    vid = request.args.get("vid")
+    if vid!= None:
+        response = firebase.fuel_volume(vid)
+        vehicle =  response['vehicle']
 
+        tank_type =["fueltank_shape"]
+
+        if tank_type == "Cuboid":
+            b =  int(vehicle["breadth"])
+            l =  int(vehicle["length"])
+            h = int(vehicle["height"]) - int(response["fuel_level"])
+
+            return {"volume": l*b*h/1000}
+        else:
+            d = int(vehicle["diameter"])
+            h = int(vehicle["height"]) - int(response["fuel_level"])
+
+            return {"volume": (math.pi * d**2 * h) / 4000}
+
+    
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
